5a43cbd354b7b4aac30a61503dc153645aac122f
Will Cohen <chewnoill@gmail.com>
2022-12-01 10:38:29 -0500

    [chore] simplify with awk

--- 01/solve.sql
+++ 01/solve.sql
@@ -1,35 +1,16 @@
 create schema if not exists day1;
 drop table day1.input;
-create table day1.input (line SERIAL, text text);
-copy day1.input (text)
-from '/workspace/01/input.txt' DELIMITER ' ';
+create table day1.input (line SERIAL, elf_id int, amount int);
 
-with elfs_ids as (
-    select *
-    from day1.input
-    where text = ''
-    UNION
-    select 0 as line,
-        '' as text
-),
-elfs as (
-    select rank() over (
-            order by line
-        ) as elf_id,
-        line as start_line,
-        lead(line, 1) over (
-            order by line
-        ) as end_line
-    from elfs_ids
-),
-part_1_answer as (
+copy day1.input (elf_id, amount)
+from PROGRAM 'awk ''BEGIN {RS = "\n\n"} {for (i = 1; i <= NF; i++) print NR,$i}'' /workspace/01/input.txt' DELIMITER ' ';
+
+with part_1_answer as (
     select elf_id,
-        sum(text::int) as total
-    from elfs
-        join day1.input on input.line > elfs.start_line
-        and input.line < elfs.end_line
+        sum(amount) as total
+    from day1.input
     group by elf_id
-    order by sum(text::int) desc
+    order by sum(amount) desc
 ),
 part_2_answer as (
     with top_3 as (
@@ -44,8 +25,8 @@ select (
         select total as "part 1 answer"
         from part_1_answer
         limit 1
-    ) (
+    ), (
         select total as "part 2 answer"
         from part_2_answer
-    ),
+    )
 ;
